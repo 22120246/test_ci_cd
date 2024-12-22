1c9f0dbf8a345766eccadb9c2eb2dea00971a065
22120246 <nhanrunandgo@gmail.com>
2024-12-22 16:49:08 +0700

    Add new ver

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Flask App 22120246"
+    return "Flask App 22120246 v1.0"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
