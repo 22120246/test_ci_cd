79ffb4b756e58966d309a8df76a4cd236f84813a
22120246 <nhanrunandgo@gmail.com>
2024-12-22 16:50:55 +0700

    Add revert

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Flask App 22120246 v1.0"
+    return "Flask App 22120246"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
